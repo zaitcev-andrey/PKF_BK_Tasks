3397242a0d431195646348840105e4355bd02a9f
Зайцев-Андрей <andrey_zaycev_2000@mail.ru>
2023-07-18 11:23:33 +0300

    Added different colors to new objects

--- Interface/Assets/Scripts/CreatePrefab.cs
+++ Interface/Assets/Scripts/CreatePrefab.cs
@@ -19,6 +19,9 @@ public class CreatePrefab : MonoBehaviour
             GameObject obj = Instantiate(_prefab, _spawnZone.position + new Vector3( Random.Range(-_radiusOfSpawnZone, _radiusOfSpawnZone), 5f,
                 Random.Range(-_radiusOfSpawnZone, _radiusOfSpawnZone)), Quaternion.identity);
 
+            Renderer rend = obj.GetComponent<Renderer>();
+            Color color = new Color((float)Random.Range(0, 255) / 255, (float)Random.Range(0, 255) / 255, (float)Random.Range(0, 255) / 255);
+            rend.material.color = color;
             _interfaceManager.AddNewObjectToList(obj);
         }
     }
